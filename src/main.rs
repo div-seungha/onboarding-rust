dcdc1eac93808a10b677c319658a05f18f19ecf1
Seungha Kim <div@cluml.com>
2024-04-25 15:21:51 +0900

    Solve all problems related to 'string' (#3)
    - add draft solutions in this repository
    - add a function to print answers on a terminal

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use rust_test::number::kids_with_candies;
 use rust_test::number::minimum_sum;
+use rust_test::string::count_matches;
+use rust_test::string::defang_i_paddr;
+use rust_test::string::most_words_found;
+use rust_test::string::num_jewels_in_stones;
+use rust_test::string::sort_sentence;
 use rust_test::vector::build_array;
 use rust_test::vector::get_concatenation;
 use rust_test::vector::maximum_wealth;
@@ -28,6 +33,51 @@ fn main() {
         println!("Answer 5: 아직 정답을 찾지 못함");
     }
 
+    fn print_string_answer() {
+        println!("*****String****************************");
+        println!(
+            "Answer 1: {:?}",
+            defang_i_paddr("101.204.305.506".to_string())
+        );
+        println!(
+            "Answer 2: {:?}",
+            num_jewels_in_stones("aBcEX".to_string(), "aaaCCBBBX".to_string())
+        );
+        println!(
+            "Answer 3: {:?}",
+            most_words_found(vec![
+                "alice and bob love leetcode".to_string(),
+                "i think so too".to_string(),
+                "this is great thanks very much".to_string()
+            ])
+        );
+        println!(
+            "Answer 4: {:?}",
+            sort_sentence("sentence4 a3 is2 This1".to_string())
+        );
+        println!(
+            "Answer 5: {:?}",
+            count_matches(
+                vec![
+                    vec!["phone".to_string(), "blue".to_string(), "pixel".to_string()],
+                    vec![
+                        "computer".to_string(),
+                        "silver".to_string(),
+                        "lenovo".to_string()
+                    ],
+                    vec![
+                        "phone".to_string(),
+                        "gold".to_string(),
+                        "iphone".to_string()
+                    ]
+                ],
+                "color".to_string(),
+                "silver".to_string()
+            )
+        );
+    }
+
     print_vector_answer();
     print_number_answer();
+    print_string_answer();
 }
