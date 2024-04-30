304f7d04adae3fa76ef2b25d19e4073ec13b61cd
Div <div@cluml.com>
2024-04-30 11:02:06 +0900

    Refactor 'vector' section codes
    - Resolved the points of PR reviews
    - Refactor remaining 3 answers on 'vector' section

--- src/leetcode/vector.rs
+++ src/leetcode/vector.rs
@@ -11,51 +11,45 @@ pub fn get_concatenation(nums: Vec<i32>) -> Vec<i32> {
 #[must_use]
 pub fn build_array(nums: Vec<i32>) -> Vec<i32> {
     nums.iter()
-        .map(|&x| *nums.get(x as usize).unwrap_or(&0))
+        .filter_map(|&x| nums.get(x as usize).cloned())
         .collect()
 }
 
 // Vector - 3
 #[must_use]
-pub fn running_sum(nums: &[i32]) -> Vec<i32> {
-    let size = nums.len();
-    let mut ans: Vec<i32> = vec![];
-    ans.push(nums[0]);
-    for i in 1..size {
-        ans.push(ans[i - 1] + nums[i]);
-    }
+pub fn running_sum(nums: Vec<i32>) -> Vec<i32> {
+    let mut ans = Vec::with_capacity(nums.len());
+    let mut sum = 0;
+    nums.iter()
+        .map(|&x| {
+            sum += x;
+            ans.push(sum);
+        })
+        .last();
+
     ans
 }
 
 // Vector - 4
 #[must_use]
 pub fn maximum_wealth(accounts: Vec<Vec<i32>>) -> i32 {
-    let mut sum_arr: Vec<i32> = vec![];
-    for item in accounts {
-        sum_arr.push(item.iter().sum())
-    }
+    let sum_arr: Vec<i32> = accounts
+        .iter()
+        .map(|account| account.iter().sum())
+        .collect();
+
     *sum_arr.iter().max().unwrap()
 }
 
 // Vector - 5
 pub fn shuffle(nums: Vec<i32>, _n: i32) -> Vec<i32> {
-    let len: usize = nums.len();
-    let len_half: usize = nums.len() / 2;
-    let mut ans = vec![];
+    let len_half = nums.len() / 2;
     let first = &nums[..len_half];
-    let second = &nums[len_half..len];
-    for i in 0..len_half {
-        let mut inner_arr = vec![];
-        if i < len_half {
-            inner_arr.push(first[i]);
-            inner_arr.push(second[i]);
-            ans.push(inner_arr);
-        } else {
-            inner_arr.push(first[i - 1]);
-            inner_arr.push(second[i - 1]);
-            ans.push(inner_arr);
-        }
-    }
+    let second = &nums[len_half..nums.len()];
 
-    ans.into_iter().flatten().collect()
+    first
+        .iter()
+        .zip(second.iter())
+        .flat_map(|(&x, &y)| vec![x, y])
+        .collect()
 }
