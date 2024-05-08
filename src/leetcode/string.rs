b04a94b115d6a073638ba6049090a77554496b35
Div <div@cluml.com>
2024-05-09 07:23:19 +0900

    Refactor remaining codes in 'string' category
    - I tried to use 'match' in a wider range.
    - But I need to study and practice to control pattern matching.
    - I found using 'match' is very useful to resolve some complex logic.

--- src/leetcode/string.rs
+++ src/leetcode/string.rs
@@ -22,52 +22,38 @@ pub fn most_words_found(sentences: Vec<String>) -> Option<usize> {
 // String - 4
 #[must_use]
 pub fn sort_sentence(s: &str) -> String {
-    let mut a = s
-        .split_whitespace()
-        .collect::<Vec<_>>()
-        .iter()
+    let mut words: Vec<&str> = vec![""; s.split_whitespace().count()];
+
+    s.split_whitespace()
         .map(|word| {
-            let c = word.rsplit("").collect::<Vec<&str>>().to_vec()[1];
-            // 아래에서 c를 숫자 타입으로 변환시켜서 sort를 돌리려고 했는데
-            // 아무리 머리를 굴리고 검색을 해 봐도 unwrap() 메서드 말고는 생각이 안 납니다...
-            // 그런데 여기서 unwrap()을 쓰면, 윗줄에서 split_whitespace로 쪼갠 단어들을 다시 거꾸로 알파벳별로 쪼개서 벡터로 만드는데
-            // 1번 인덱스를 사용한 이유는 println!을 써서 실제로 rsplit의 결과물을 확인한 다음에 넣은 코드이지만
-            // 1번 인덱스에 parse할 수 있는 무엇이 없으면 패닉에 빠지므로 이것조차 완전한 방법이 아닙니다...ㅠㅠ
-            let t_data = (&word[..word.len() - 1], c.parse::<usize>().unwrap());
-            t_data
+            let a = word.chars().last();
+            let b = (match a {
+                Some(v) => v,
+                None => ' ',
+            })
+            .to_digit(10);
+
+            let c = match b {
+                Some(v) => v as usize,
+                None => 0,
+            };
+
+            (&word[..word.len() - 1], c - 1)
         })
-        .collect::<Vec<(&str, usize)>>();
+        .for_each(|(word, i)| words[i] = word);
 
-    a.sort_by_key(|&(_word, num)| num);
-
-    a.iter()
-        .map(|(word, _)| word.to_string())
-        .collect::<Vec<String>>()
-        .join(" ")
+    words.join(" ")
 }
 
 // String - 5
 #[must_use]
-pub fn count_matches(items: Vec<Vec<String>>, rule_key: &str, rule_value: &str) -> i32 {
-    let mut ans_v = vec![0; items.len()];
-    let mut ans = 0;
+pub fn count_matches(items: Vec<Vec<String>>, rule_key: &str, rule_value: &str) -> usize {
+    let idx = match rule_key {
+        "type" => 0,
+        "color" => 1,
+        "name" => 2,
+        _ => panic!("Something is wrong."),
+    };
 
-    for item in items {
-        let mut matching_items = 0;
-        if (rule_key == "type" && item[0] == rule_value)
-            || (rule_key == "color" && item[1] == rule_value)
-            || (rule_key == "name" && item[2] == rule_value)
-        {
-            matching_items += 1;
-        }
-        ans_v.push(matching_items);
-    }
-
-    for i in ans_v {
-        if i > 0 {
-            ans += 1;
-        }
-    }
-
-    ans
+    items.iter().filter(|item| item[idx] == rule_value).count()
 }
