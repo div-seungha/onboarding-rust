e27083677cdd9a4034000ebcc505503f47fbdb7a
seungha <serendipity@beonanotherplanet.com>
2024-05-10 11:00:36 +0900

    Refactor with in cosideraton of what I reviewed
    - I used , and tried to use error handling method much more than before.

--- src/leetcode/number.rs
+++ src/leetcode/number.rs
@@ -1,14 +1,38 @@
 // Number - 1
 #[must_use]
 pub fn minimum_sum(num: i32) -> i32 {
-    let mut digits = [num % 10, num / 10 % 10, num / 100 % 10, num / 1000 % 10];
-    digits.sort_by_key(|&x| x);
+    fn is_four_digit(num: i32) -> Result<i32, String> {
+        if num >= 1000 && num <= 9999 {
+            Ok(num)
+        } else {
+            Err(format!(
+                "{} This function cannot control this parameter:",
+                num
+            ))
+        }
+    }
+
+    let mut digit = match is_four_digit(num) {
+        Ok(v) => v,
+        Err(e) => {
+            println!("{}", e);
+            0
+        }
+    };
+
+    let unit = vec![1000, 100, 10, 1];
+    let mut digits = vec![];
+
+    for item in unit {
+        digits.push(digit / item);
+        digit = digit % item;
+    }
 
     (digits[0] * 10 + digits[3]) + (digits[1] * 10 + digits[2])
 }
 
 // Number - 2
-pub fn num_identical_pairs(nums: Vec<i32>) -> i32 {
+pub fn num_identical_pairs(nums: Vec<u32>) -> u32 {
     use std::collections::HashMap;
 
     nums.iter()
@@ -17,11 +41,25 @@ pub fn num_identical_pairs(nums: Vec<i32>) -> i32 {
                 b += c;
             }
 
-            *a.entry(num).or_insert(0) += 1;
+            a.entry(num).and_modify(|v: &mut u32| *v += 1);
 
             (a, b)
         })
         .1
+}
+
+// Number - 2 - for
+pub fn num_identical_pairs_with_for(nums: Vec<u32>) -> u32 {
+    let mut count = 0;
+
+    for i in 0..nums.len() {
+        for j in i + 1..nums.len() {
+            if nums[i] == nums[j] {
+                count += 1;
+            }
+        }
+    }
+    count
 }
 
 // Number - 3
@@ -31,23 +69,29 @@ pub fn kids_with_candies(candies: &[i32], extra_candies: i32) -> Vec<bool> {
 
     let c = match max_candies {
         Some(v) => v,
-        None => &0,
+        None => {
+            format!("Something is wrong.");
+            &0
+        }
     };
 
     candies
         .iter()
-        .map(|&candy| candy + extra_candies >= *c) // Check if each child can have the most candies
+        .map(|&candy| candy + extra_candies >= *c)
         .collect()
 }
 
 // Number - 4
 #[must_use]
 pub fn subtract_product_and_sum(n: i32) -> u32 {
-    let v = n.to_string();
-    let digits = v.chars().filter_map(|c| c.to_digit(10)).map(|d| d);
+    let digits: Vec<u32> = n
+        .to_string()
+        .chars()
+        .filter_map(|c| Some(c.to_digit(10).expect("Returns None, Something is wrong.")))
+        .collect();
 
-    let product: &u32 = &digits.clone().product();
-    let sum: &u32 = &digits.sum();
+    let product: u32 = digits.iter().product();
+    let sum: u32 = digits.iter().sum();
 
     product - sum
 }
@@ -56,6 +100,6 @@ pub fn subtract_product_and_sum(n: i32) -> u32 {
 #[must_use]
 pub fn smaller_numbers_than_current(nums: &[i32]) -> Vec<usize> {
     nums.iter()
-        .map(|i| nums.iter().filter(|j| j < &i).count())
+        .map(|i| nums.iter().filter(|j| *j < i).count())
         .collect()
 }
