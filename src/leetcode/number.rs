5e88868e62455b22622e67ee67bf54226cefee13
Div <div@cluml.com>
2024-05-13 10:05:52 +0900

    Refactor codes with considering what I got reviewed
    - Modified error messages of `Return<T, E>` more user-friendly
    - Modified the second answer with using `for`, `entry` and `and_modify`

--- src/leetcode/number.rs
+++ src/leetcode/number.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 // Number - 1
 #[must_use]
 pub fn minimum_sum(num: i32) -> Result<i32, String> {
@@ -12,47 +14,32 @@ pub fn minimum_sum(num: i32) -> Result<i32, String> {
         Ok((digits[0] * 10 + digits[3]) + (digits[1] * 10 + digits[2]))
     } else {
         Err(format!(
-            "{} This function cannot control this parameter:",
+            "{} The input of this function must be 4 decimal digits. So this function cannot control this input:",
             num
         ))
     }
 }
 
-// Number - 2 -- 현재 풀이 중 ---------------------------------------------
-// pub fn num_identical_pairs(nums: Vec<u32>) -> u32 {
-//     use std::collections::HashMap;
-
-//     for n in nums {
-//         let t = (HashMap::new(), 0);
-//         let v = 0;
-//     }
-
-//     nums.iter()
-//         .fold((HashMap::new(), 0), |(mut a, mut b), &num| {
-//             if let Some(&c) = a.get(&num) {
-//                 b += c;
-//             }
-
-//             a.entry(num).and_modify(|v| *v += 1);
-
-//             (a, b)
-//         })
-//         .1
-// }
-
 // Number - 2 - for
-// pub fn num_identical_pairs_with_for(nums: Vec<u32>) -> u32 {
-//     let mut map = HashMap::new();
-//     let mut t = (map, 0);
+pub fn num_identical_pairs_with_for(nums: Vec<u32>) -> u32 {
+    let mut map = HashMap::new();
+    let mut count = 0;
 
-//     for i in 0..nums.len() - 1 {
-//         if nums[i] == map.get(i) {
-//             t.entry(nums[i]).and_modify(|counter| *counter += 1);
-//         }
-//     }
+    for &num in &nums {
+        let entry = map
+            .entry(num)
+            .and_modify(|e| {
+                count += *e;
+                *e += 1
+            })
+            .or_insert(0);
+        if *entry == 0 {
+            *entry += 1;
+        }
+    }
 
-//     10
-// }
+    count
+}
 // -------------------------------------------------------------------
 
 // Number - 3
@@ -65,7 +52,7 @@ pub fn kids_with_candies(candies: &[i32], extra_candies: i32) -> Result<Vec<bool
             .collect())
     } else {
         Err(format!(
-            "{:?} There is none of max value in candies. Something is wrong.",
+            "{:?} To solve this problem, the first input must have one maximum value. There is none of one maximum value in candies.",
             candies
         ))
     }
@@ -73,15 +60,21 @@ pub fn kids_with_candies(candies: &[i32], extra_candies: i32) -> Result<Vec<bool
 
 // Number - 4
 #[must_use]
-pub fn subtract_product_and_sum(n: i32) -> u32 {
-    let digits: Vec<u32> = n
+pub fn subtract_product_and_sum(n: i32) -> i32 {
+    let digits: Vec<i32> = n
         .to_string()
         .chars()
-        .filter_map(|c| Some(c.to_digit(10).expect("Returns None, Something is wrong.")))
-        .collect();
+        .filter_map(|c| {
+            Some(
+                c.to_digit(10)
+                    .map(|v| v as i32)
+                    .expect("Returns None, Something is wrong."),
+            )
+        })
+        .collect::<Vec<i32>>();
 
-    let product: u32 = digits.iter().product();
-    let sum: u32 = digits.iter().sum();
+    let product: i32 = digits.iter().product();
+    let sum: i32 = digits.iter().sum();
 
     product - sum
 }
